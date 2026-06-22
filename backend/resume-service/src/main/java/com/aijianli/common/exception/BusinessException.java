d1a5fb78680c3da47401ae7cdbb7d7b77f63c31f
zzzzz681 <1418228103@qq.com>
2026-06-22 10:51:24 +0800

    Add business exception

---
+++ backend/resume-service/src/main/java/com/aijianli/common/exception/BusinessException.java
@@ -0,0 +1,28 @@
+package com.aijianli.common.exception;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+
+    private final int code;
+    private final String message;
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.code = errorCode.getCode();
+        this.message = errorCode.getMessage();
+    }
+
+    public BusinessException(ErrorCode errorCode, String message) {
+        super(message);
+        this.code = errorCode.getCode();
+        this.message = message;
+    }
+
+    public BusinessException(int code, String message) {
+        super(message);
+        this.code = code;
+        this.message = message;
+    }
+}
