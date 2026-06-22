0a6bbca830e63c287fb710fd3f7bd02fa31bef44
zzzzz681 <1418228103@qq.com>
2026-06-22 10:50:12 +0800

    Add role constants

---
+++ backend/resume-service/src/main/java/com/aijianli/common/constant/RoleConstant.java
@@ -0,0 +1,11 @@
+package com.aijianli.common.constant;
+
+public final class RoleConstant {
+
+    public static final String ROLE_USER = "ROLE_USER";
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+    public static final String ROLE_SUPER_ADMIN = "ROLE_SUPER_ADMIN";
+
+    private RoleConstant() {
+    }
+}
